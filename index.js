05a1d0d26e39fb6c1e191f5646b0b9301382b207
damau <2249211+damau@users.noreply.github.com>
2021-12-03 01:20:41 +0800

    Need to fix left pull out.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react"
 import {
   Animated,
   useWindowDimensions,
@@ -7,83 +7,91 @@ import {
   Text,
   SafeAreaView,
   Platform,
-  TouchableOpacity,
-} from "react-native";
-import PropTypes from "prop-types";
+  TouchableOpacity
+} from "react-native"
+import PropTypes from "prop-types"
 
-const MenuDrawer = (props) => {
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-  const leftOffsetRef = useRef(new Animated.Value(0));
-  const isLeftPosition = props.position === "left";
-  const window = useWindowDimensions();
-  const screenHeight = window.height;
-  const screenWidth = window.width;
-  const drawerWidthRef = useRef(screenWidth * (props.drawerPercentage / 100));
+const MenuDrawer = props => {
+  const fadeAnim = useRef(new Animated.Value(0)).current
+  const isLeftPosition = props.position === "left"
+  const window = useWindowDimensions()
+  const screenHeight = window.height
+  const screenWidth = window.width
 
+  const initialDrawerWidth = screenWidth * (props.drawerPercentage / 100)
+  const drawerWidthRef = useRef(initialDrawerWidth)
 
-  useEffect(() => {
-    drawerWidthRef.current = screenWidth * (props.drawerPercentage / 100);
+  const initialLeftOffset = isLeftPosition ? 0 : screenWidth + initialDrawerWidth
+  const leftOffsetRef = useRef(new Animated.Value(initialLeftOffset))
 
-    leftOffsetRef.current = new Animated.Value(0);
-
-    if(props.open) { closeDrawer() & openDrawer() }
-  }, [screenWidth]);
 
   useEffect(() => {
-    props.open ? openDrawer() : closeDrawer();
-  }, [props.open]);
+    const newDrawerWidth = screenWidth * (props.drawerPercentage / 100)
+    const newLeftOffset = isLeftPosition ? 0 : screenWidth + newDrawerWidth
+    drawerWidthRef.current = newDrawerWidth
+
+    leftOffsetRef.current = new Animated.Value(newLeftOffset)
+
+    if (props.open) {
+      closeDrawer() & openDrawer()
+    }
+  }, [screenWidth])
+
+  useEffect(() => {
+    props.open ? openDrawer() : closeDrawer()
+  }, [props.open])
 
   const openDrawer = () => {
-    const { drawerPercentage, animationTime, opacity, position } = props;
-    const drawerWidth = drawerWidthRef.current;
-    const leftOffset = leftOffsetRef.current;
+    const { drawerPercentage, animationTime, opacity, position } = props
+    const drawerWidth = drawerWidthRef.current
+    const leftOffset = leftOffsetRef.current
 
     Animated.parallel([
       Animated.timing(leftOffset, {
         toValue: isLeftPosition ? drawerWidth : screenWidth,
         duration: animationTime,
-        useNativeDriver: true,
+        useNativeDriver: true
       }),
       Animated.timing(fadeAnim, {
         toValue: opacity,
         duration: animationTime,
-        useNativeDriver: true,
-      }),
-    ]).start();
-  };
+        useNativeDriver: true
+      })
+    ]).start()
+  }
 
   const closeDrawer = () => {
-    const { drawerPercentage, animationTime, position } = props;
-    const drawerWidth = drawerWidthRef.current;
-    const leftOffset = leftOffsetRef.current;
+    const { drawerPercentage, animationTime, position } = props
+    const drawerWidth = drawerWidthRef.current
+    const leftOffset = leftOffsetRef.current
 
     Animated.parallel([
       Animated.timing(leftOffset, {
         toValue: isLeftPosition ? 0 : screenWidth + drawerWidth,
         duration: animationTime,
-        useNativeDriver: true,
+        useNativeDriver: true
       }),
       Animated.timing(fadeAnim, {
         toValue: 1,
         duration: animationTime,
-        useNativeDriver: true,
-      }),
-    ]).start();
-  };
+        useNativeDriver: true
+      })
+    ]).start()
+  }
 
   const drawerFallback = () => {
     return (
       <TouchableOpacity onPress={closeDrawer}>
         <Text>Close</Text>
       </TouchableOpacity>
-    );
-  };
+    )
+  }
 
   const renderPush = () => {
-    const { children, drawerContent, drawerPercentage } = props;
+    const { children, drawerContent, drawerPercentage } = props
     const drawerWidth = drawerWidthRef.current
     const leftOffset = leftOffsetRef.current
-    const animated = { transform: [{ translateX: leftOffset }] };
+    const animated = { transform: [{ translateX: leftOffset }] }
 
     return (
       <Animated.View style={[animated, styles.main]}>
@@ -93,8 +101,8 @@ const MenuDrawer = (props) => {
             {
               height: screenHeight,
               width: drawerWidth,
-              left: -drawerWidth,
-            },
+              left: -drawerWidth
+            }
           ]}
         >
           {drawerContent ? drawerContent : drawerFallback()}
@@ -103,21 +111,21 @@ const MenuDrawer = (props) => {
           style={[
             styles.container,
             {
-              opacity: fadeAnim,
-            },
+              opacity: fadeAnim
+            }
           ]}
         >
           {children}
         </Animated.View>
       </Animated.View>
-    );
+    )
   }
 
   const renderOverlay = () => {
-    const { children, drawerContent, drawerPercentage } = props;
+    const { children, drawerContent, drawerPercentage } = props
     const drawerWidth = drawerWidthRef.current
     const leftOffset = leftOffsetRef.current
-    const animated = { transform: [{ translateX: leftOffset }] };
+    const animated = { transform: [{ translateX: leftOffset }] }
 
     return (
       <>
@@ -125,22 +133,17 @@ const MenuDrawer = (props) => {
           style={[
             animated,
             styles.drawer,
-            { height: screenHeight,
-              width: drawerWidth,
-              left: -drawerWidth },
-          ]}>
+            { height: screenHeight, width: drawerWidth, left: -drawerWidth }
+          ]}
+        >
           {drawerContent ? drawerContent : drawerFallback()}
         </Animated.View>
-        <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
-          {children}
-        </Animated.View>
+        <Animated.View style={[styles.container, { opacity: fadeAnim }]}>{children}</Animated.View>
       </>
-    );
+    )
   }
 
-  return(
-    props.overlay ? renderOverlay() : renderPush()
-  )
+  return props.overlay ? renderOverlay() : renderPush()
 }
 
 MenuDrawer.defaultProps = {
@@ -149,8 +152,8 @@ MenuDrawer.defaultProps = {
   animationTime: 200,
   overlay: true,
   opacity: 0.4,
-  position: "left",
-};
+  position: "left"
+}
 
 MenuDrawer.propTypes = {
   open: PropTypes.bool,
@@ -158,23 +161,23 @@ MenuDrawer.propTypes = {
   animationTime: PropTypes.number,
   overlay: PropTypes.bool,
   opacity: PropTypes.number,
-  position: PropTypes.oneOf(["left", "right"]),
-};
+  position: PropTypes.oneOf(["left", "right"])
+}
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
     left: 0,
-    top: 0,
+    top: 0
   },
   container: {
     flex: 1,
-    zIndex: 0,
+    zIndex: 0
   },
   drawer: {
     position: "absolute",
-    zIndex: 1,
-  },
-});
+    zIndex: 1
+  }
+})
 
-export default MenuDrawer;
+export default MenuDrawer
